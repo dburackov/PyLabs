80fe42d44c402aee9be48d3d184285b7c5687302
Dmitry Burackov <dmburackov@yandex.by>
2022-05-27 17:14:21 +0300

    add main view

---
+++ lab3/animecal/main/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import View
+
+
+def index(request):
+    return HttpResponse("<h2>FDFFDF</h2>")
+"""""
+class MainView(View):
+    def get(self, request):
+        pass
+
+
+    def psot(self, request):
+        pass
+
+
+    def 
+"""
