b3fc341a0c4580fac6a8b8d2a2fe21bd677e69d0
Dmitry Burackov <dmburackov@yandex.by>
2022-05-28 21:28:58 +0300

    add models

--- lab3/animecal/main/models.py
+++ lab3/animecal/main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 
+class Animes(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=False)
+    poster = models.ImageField(upload_to="photos/")
+    data = models.DateField()
+    episodes = models.IntegerField()
+
+
+class Comments(models.Model):
+    author = models.CharField(max_length=255)
+    text = models.TextField(blank=False)
+    data = models.DateTimeField(auto_now_add=True)
