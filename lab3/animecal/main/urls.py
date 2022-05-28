323d874f1c3302b60a13455494a023f6f7c9a71f
Dmitry Burackov <dmburackov@yandex.by>
2022-05-28 21:03:33 +0300

    login logout signout full done with css styles

--- lab3/animecal/main/urls.py
+++ lab3/animecal/main/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', index, name='anime_list'),
     path('about', about, name='about'),
     path('register', RegisterUser.as_view(), name='register'),
-
+    path('login', LoginUser.as_view(), name='login'),
+    path('logout', logout_user, name='logout'),
 ]
 
--- lab3/animecal/main/views.py
+++ lab3/animecal/main/views.py
@@ -1,5 +1,7 @@
-from .forms import RegisterUserForm
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.contrib.auth.views import LoginView
+from .forms import RegisterUserForm, LoginUserForm
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import View, CreateView
 
@@ -26,3 +28,15 @@ class RegisterUser(CreateView):
 
     def get_user_context(self):
         """
+
+class LoginUser(LoginView):
+    form_class = LoginUserForm
+    template_name = "main/login.html"
+
+    def get_success_url(self):
+        return reverse_lazy('anime_list')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
